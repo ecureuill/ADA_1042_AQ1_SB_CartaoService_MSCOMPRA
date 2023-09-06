e26334e40fdd5d08b087eceb816c4fa8d7255927
leofss <leofernandess11@gmail.com>
2023-09-06 10:35:50 -0300

    Fix: booting up starting files

---
+++ src/main/java/com/ada/grupo5/mscompra/MscompraApplication.java
@@ -0,0 +1,13 @@
+package com.ada.grupo5.mscompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscompraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ada/grupo5/mscompra/MscompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.grupo5.mscompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
