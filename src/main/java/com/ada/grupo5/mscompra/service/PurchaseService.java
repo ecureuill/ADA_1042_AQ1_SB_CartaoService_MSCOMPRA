a7875ecc463f1592642ef919711f23f34b8b5057
leofss <leofernandess11@gmail.com>
2023-09-07 21:58:29 -0300

    Feat: validate date

--- src/main/java/com/ada/grupo5/mscompra/service/PurchaseService.java
+++ src/main/java/com/ada/grupo5/mscompra/service/PurchaseService.java
@@ -3,7 +3,13 @@ package com.ada.grupo5.mscompra.service;
 import com.ada.grupo5.mscompra.dto.PurchaseDto;
 import com.ada.grupo5.mscompra.entity.Purchase;
 import com.ada.grupo5.mscompra.repository.PurchaseRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Service
 public class PurchaseService {
@@ -14,10 +20,20 @@ public class PurchaseService {
         this.purchaseRepository = purchaseRepository;
     }
 
+    private boolean validateDate(LocalDate expireCardDate, LocalDateTime purchaseDate){
+        return purchaseDate.isBefore(expireCardDate.atStartOfDay());
+    }
+
     public PurchaseDto createPurchase(PurchaseDto purchaseDto){
         Purchase purchase = new Purchase(purchaseDto);
-        purchase = this.purchaseRepository.save(purchase);
-        return purchase.PurchaseToDto();
+        boolean validateDate = validateDate(purchase.getExpireCardDate(), purchase.getPurchaseDate());
+
+        if(validateDate){
+            purchase = this.purchaseRepository.save(purchase);
+            return purchase.PurchaseToDto();
+        }else{
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cartão expirado na data de compra");
+        }
 
     }
 }
